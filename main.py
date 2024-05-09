2e4e1705658fe1e37a81bde8a2efbedc6bf91577
lebretonl <lebreton.l@outlook.com>
2024-05-09 13:48:36 +0200

    affichage des réponses , ajout boutton pour vider le csv

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import csv
-from fastapi.staticfiles import StaticFiles
-
+from fastapi.responses import FileResponse
+from fastapi.responses import RedirectResponse
 
 # Création d'une instance de l'application FastAPI
 app = FastAPI()
 
-# Configuration pour les fichiers statiques (icone)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+favicon_path = 'static/favicon.ico'
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
 
 # Configuration pour les templates
 templates = Jinja2Templates(directory="templates")
@@ -20,9 +23,32 @@ def form(request: Request):
     return templates.TemplateResponse("formulaire.html", {"request": request})
 
 
+# Lors de la soumission du formulaire, les données sont envoyées en POST, et on affiche les réponses
 @app.post("/submit")
-def submit_form(pcs: str = Form(...)):
+def submit_form(request: Request,pcs: str = Form(...), sector: str = Form(...), nomPME: str = Form(...)):
     with open('reponses.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([pcs])
-    return {"message": "Réponse enregistrée avec succès"}
+        writer.writerow(['Nom PME: ' + nomPME])
+        writer.writerow(['1. Nombre de pc: ' + pcs])
+        writer.writerow(['2. Secteur activite: ' + sector])
+        writer.writerow(['-------------------'])
+    
+    with open('reponses.csv', 'r') as file:
+        data = list(csv.reader(file))
+
+    return templates.TemplateResponse("affichage-reponse.html", {"request": request, "data": data})
+
+# Permet de voir les réponses déjà soumises
+@app.get("/submit")
+def get_submit_form(request: Request):
+    with open('reponses.csv', 'r') as file:
+        data = list(csv.reader(file))
+
+    return templates.TemplateResponse("affichage-reponse.html", {"request": request, "data": data})
+
+# Efface le contenu du fichier CSV
+# Redirige vers la page affichage-reponse.html pour afficher le contenu du fichier CSV (qui est vide)
+@app.get("/clear")
+def clear_csv(request: Request):
+    open('reponses.csv', 'w').close()  
+    return RedirectResponse(url="/submit")
