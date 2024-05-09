df4d087994bc2cade510422eec564aece0a6d176
lebretonl <lebreton.l@outlook.com>
2024-05-09 09:51:17 +0200

    ajout css

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# Importation de FastAPI
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+import csv
 
 
 # Création d'une instance de l'application FastAPI
@@ -14,3 +14,15 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def form(request: Request):
     return templates.TemplateResponse("formulaire.html", {"request": request})
+
+
+@app.post("/submit")
+def submit_form(pcs: str = Form(...)):
+    with open('reponses.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([pcs])
+    return {"message": "Réponse enregistrée avec succès"}
+
+@app.get("/test", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
