172a4bd16ce95d4273ff10351d0b94d206cb1137
lebretonl <lebreton.l@outlook.com>
2024-05-09 09:16:41 +0200

    ajout formulaire

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # install fastAPI 
 pip install fastapi[all]
 
+# install uvicorn (server)
+pip install uvicorn
+
 # start server
 uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # Importation de FastAPI
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 # Création d'une instance de l'application FastAPI
 app = FastAPI()
 
+# Configuration pour les templates
+templates = Jinja2Templates(directory="templates")
+
 # Définition d'une route root qui répond à une requête GET
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+def form(request: Request):
+    return templates.TemplateResponse("formulaire.html", {"request": request})
